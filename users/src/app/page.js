183e4f82ed339648eb6d136d161823e841f7c3f5
sharaf kamaal <sharafkamaal@gmail.com>
2025-08-28 13:34:05 +0530

    use client added

--- users/src/app/page.js
+++ users/src/app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -46,15 +47,15 @@ function LoginForm() {
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
+      <main classNamflex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        {/* <Imagee="
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
-        />
+        /> */}
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
@@ -75,13 +76,13 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
+            {/* <Image
               className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
-            />
+            /> */}
             Deploy now
           </a>
           <a
@@ -102,13 +103,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
+          {/* <Image
             aria-hidden
             src="/file.svg"
             alt="File icon"
             width={16}
             height={16}
-          />
+          /> */}
           Learn
         </a>
         <a
@@ -117,13 +118,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
+          {/* <Image
             aria-hidden
             src="/window.svg"
             alt="Window icon"
             width={16}
             height={16}
-          />
+          /> */}
           Examples
         </a>
         <a
@@ -132,16 +133,16 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
+          {/* <Image
             aria-hidden
             src="/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
-          />
+          /> */}
           Go to nextjs.org →
         </a>
       </footer>
-    </div>
+    </div >
   );
 }
